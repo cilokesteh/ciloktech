3891771629d067d76991945cc8f176093cd99a90
cilokesteh <cilokesteh@users.noreply.github.com>
2026-08-10 12:01:50 +0800

    schema: update priceRange to Rp 900rb - Rp 2jt+

--- app/layout.tsx
+++ app/layout.tsx
@@ -131,7 +131,7 @@ export default function RootLayout({
                 addressCountry: "ID",
                 addressLocality: "Indonesia",
               },
-              priceRange: "Rp 900rb - Rp 15jt+",
+              priceRange: "Rp 900rb - Rp 2jt+",
               openingHours: "Mo-Su",
             }),
           }}
